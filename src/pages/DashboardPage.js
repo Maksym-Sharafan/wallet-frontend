490fa66d56a28df7ed2930183d18910a83137610
Maksym Sharafan <73744713+Maksym-Sharafan@users.noreply.github.com>
2021-11-21 14:59:00 +0200

    Update DashboardPage.js

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -5,17 +5,21 @@ import AppBar from '../components/AppBar';
 import Header from '../components/Header';
 
 import styles from './DashboardPage.module.css';
-import React from 'react';
+// import { transactionsOperations } from "../redux/transactions";
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
 
-export default function DashboardPage({ children }) {
+export default function DashboardPage() {
+      //     const dispatch = useDispatch();
+    //     useEffect(() => {
+    //     dispatch(transactionsOperations.fetchTransactions());
+    //   }, [dispatch]);
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <div className={styles.main_container}>
-         
+        <div className={styles.main_container}>        
           <AppBar />
-         
           <div className={styles.main_content}>
             <Outlet />
           </div>
