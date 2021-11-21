9e87e9dc7d57f4f515312dbda243ac48a7ac7845
Serhii <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-11-21 13:26:50 +0200

    translate text and fix code

--- src/components/Statistics/DiagramTab/monthAndYear.js
+++ src/components/Statistics/DiagramTab/monthAndYear.js
@@ -1,14 +1,14 @@
 const currentYear = new Date().getFullYear();
-const currentMonth = new Date().toLocaleString('en', { month: 'long' });
+const currentMonth = new Date().toLocaleString('ru', { month: 'long' });
 
 const months = new Array(12).fill(0).map((_, i) => {
-  return new Date(`${i + 1}`).toLocaleString('en', { month: 'long' });
+  return new Date(`${i + 1}`).toLocaleString('ru', { month: 'long' });
 });
 
 const range = (start, stop) =>
   Array.from({ length: stop - start }, (_, i) => (start += 1));
 
-const years = range(currentYear - 2, currentYear + 2);
+const years = range(currentYear - 4, currentYear + 0);
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { currentYear, currentMonth, months, years };
