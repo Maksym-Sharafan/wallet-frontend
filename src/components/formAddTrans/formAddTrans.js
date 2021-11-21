4a57347ea948b3d353fe1626cc2418bb6394060e
unknown <tanushaGK@i.ua>
2021-11-21 12:13:19 +0200

    add validatuin

--- src/components/formAddTrans/formAddTrans.js
+++ src/components/formAddTrans/formAddTrans.js
@@ -1,13 +1,42 @@
 import React, { useState } from 'react';
-import { useDispatch } from "react-redux";
+import { useDispatch , useSelector} from "react-redux";
 import MyDatePicker from "./dtpicer";
 import { useFormik } from 'formik';
 import styles from './formAddTrans.module.css';
+import {isModalAddTransactionOpen} from '../../redux/modalAddTransaction/modal-actions'
 import addTransaction from '../../redux/transactions/transactions-operations'
 import categoriesList from '../../redux/categories/categories'
 import CustomizedSelects from './selectMUI'
 
+const validate = values => {
+   const errors = {};
+
+  //  if (!values.category){
+  //    errors.amount= 'Добавьте категорию';
+  //  } else if(values.category.length <3 ) {
+  //    errors.category = 'Выберите категорию';
+  //  }
+ 
+   if (!values.amount) {
+     errors.amount = 'Введите сумму транзакции';
+   } else if (values.amount.length < 1) {
+     errors.amount = 'Введите сумму транзакции';
+   }
+ 
+  //  if (!values.date) {
+  //    errors.date = 'Required';
+  //  } else if (values.date.length < 1) {
+  //    errors.date = 'Укажите дату трансакции';
+  //  }
+ 
+   return errors;
+ };
+
+
 const FormAddTrans = () => {
+  const dispatch = useDispatch();
+  //данные из глобального Store о состоянии модалки
+  const ShowModal= useSelector((state) => state.global)
   // состояние для выбора типа транзакции 
 const [isChecked, setIsChecked] = useState(false);
 // сщстояние для типа трансакции
@@ -26,26 +55,31 @@ const [isSelected, setIsSelected] = useState(' ')
   console.log(isSelected)
  }
 
-
+ const handelCancel = (e)=> {
+  dispatch(isModalAddTransactionOpen());
+ }
   const options = [ ...categoriesList];
+
   const formik = useFormik({    
     initialValues: {
       type: "",
       category: isSelected,
-      amount: " ",
-      date: " ",
-      comment: ' ',
+      amount: "",
+      date: "",
+      comment: "",
     },
+    validate, 
     onSubmit: values => {
       alert(JSON.stringify({
-        ...values, type: transType, category:isSelected 
+        ...values, type: transType, category: isSelected 
       }, null, 2));
+      dispatch(isModalAddTransactionOpen());
     },
   });
   return (
-    <form onSubmit={formik.handleSubmit} className={styles.formWraper}>
+    <form onSubmit={formik.handleSubmit} className={styles.formWraper} autocompleted="false">
       <div className = {styles.header}>
-      <h1 className={styles.hText}>Добавить транзакцию</h1>
+      <h1 className={styles.hText}> Добавить транзакцию</h1>
       </div>
       <div className={styles.inputGroup}>
         <div className={styles.switchWraper}>
@@ -56,7 +90,7 @@ const [isSelected, setIsSelected] = useState(' ')
           <label className={styles.switch}>
            <input type="checkbox" className={styles.sliderInput} 
             onChange = {handleChange}/>
-             <span className={styles.slider}></span>
+            <span className={styles.slider}></span>
           </label>
           <span className={styles.cost} 
           style={{color:isChecked ? "#FF6596": "#E0E0E0"}}>Расход</span>
@@ -70,19 +104,22 @@ const [isSelected, setIsSelected] = useState(' ')
         name="category"
         required = {true}
         type="select"
-        onChange={ handelSelect} >
-          <option value="" selected  disabled hidden >
+        onChange={ handelSelect} 
+         >
+          <option value="" selected disabled hidden={true} >
             Выберите категорию</option>
          {options.map(({_id, name}) => {
           return (<option value={_id} key={_id} className={styles.option}>
             {name}
             </option>)
         })}
+          
           </select> 
           </div> 
         }
       <div className={styles.inputGroup}>
       <input className={styles.input}
+        required = {true}
         id="amount"
         name="amount"
         type="number"
@@ -92,7 +129,11 @@ const [isSelected, setIsSelected] = useState(' ')
         onChange={formik.handleChange}
         value={formik.values.amount}
       />
-      {/* <MyDatePicker /> */}
+      { formik.errors.amount ? <div className={styles.error}>{ formik.errors.amount }</div> : null }
+      {/* <MyDatePicker /> 
+      валидация для даты
+      { formik.errors.date? <div>{formik.errors.date}</div> : null }
+      */}
       </div>
       <input className={styles.comment}
         id="comment"
@@ -104,7 +145,7 @@ const [isSelected, setIsSelected] = useState(' ')
       />
       </div>
       <button type="submit" className={styles.addBtn }>Добавить</button>
-      <button type="submit" className={styles.CancelBtn}  >
+      <button type="submit" className={styles.CancelBtn} onClick={handelCancel} >
         Отмена
         </button>
     </form>
