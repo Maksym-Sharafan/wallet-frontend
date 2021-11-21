374ec07be351a91f8cf6c41c4ca55ee564052a81
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-11-21 12:58:13 +0200

    apdate checker

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -2,15 +2,23 @@ import { Outlet } from "react-router";
 import Media from 'react-media';
 
 import AppBar from "../components/AppBar";
+// import AddBtn from "../components/AddBtn";
 
 import Header from "../components/Header";
+// import { transactionsOperations } from "../redux/transactions";
 
 
 
 import styles from "./DashboardPage.module.css";
 import React from "react";
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
 
 export default function DashboardPage({ children }) {
+//     const dispatch = useDispatch();
+//     useEffect(() => {
+//     dispatch(transactionsOperations.fetchTransactions());
+//   }, [dispatch]);
 
     return (
         <>
@@ -18,6 +26,7 @@ export default function DashboardPage({ children }) {
             <main className={styles.main}>
                 <div className={styles.main_container}>
                     <AppBar />
+                    {/* <AddBtn/> */}
                     <div className={styles.main_content}>
                         <Outlet />
                     </div>
