6698be0230abc9c900e43bb6bb86eef07e295089
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-11-22 18:25:32 +0200

    add link to logo and modern navigate

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,49 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css';
 import Icons from '../../components/Icons';
-const Navigation = () => (
-  <div className={styles.navigation_container}>
-    <nav className={styles.navigation_nav_link}>
-      <NavLink to="/home" className={styles.navigation_link} end>
-        <Icons name="home" className={styles.navigation_icon_home} />
-        <p className={styles.navigation_name_home}> Главная </p>
-      </NavLink>
-      <NavLink end to="/diagram" className={styles.navigation_link}>
-        <Icons name="statistic" className={styles.navigation_icon_statistic} />
-        <p className={styles.navigation_name_statistic}>Статистика</p>
-      </NavLink>
-      <NavLink end to="/exclude" className={styles.navigation_link}>
-        <Icons name="dollar" className={styles.navigation_icon_dollar} />
-           </NavLink>
-    </nav>
-  </div>
-);
+
+// const isActiveFn2 = ({ isActive }) => {
+
+//   return isActive ? styles.active : styles.navigation_link;
+// }
+const isActiveFn = ({ isActive }) => {
+
+  return isActive ? styles.active : styles.navigation_link;
+}
+
+const Navigation = () => {
+
+  return (
+    <div className={styles.navigation_container}>
+      <nav className={styles.navigation_nav}>
+        <NavLink
+          to="/home"
+          className={isActiveFn}
+        >
+          <Icons
+            name="home"
+            className={styles.navigation_icon}
+
+          />
+          <p className={styles.navigation_name_statistic}>Главная</p>
+        </NavLink>
+        <NavLink
+          end
+          to="/diagram"
+          className={isActiveFn}
+        >
+          <Icons name="statistic" className={styles.navigation_icon} />
+          <p className={styles.navigation_name_statistic}>Статистика</p>
+        </NavLink>
+        <NavLink
+          end
+          to="/exclude"
+          className={isActiveFn}
+        >
+          <Icons name="dollar" className={styles.navigation_icon_dollar} />
+        </NavLink>
+      </nav>
+    </div>
+  )
+};
 export default Navigation;
