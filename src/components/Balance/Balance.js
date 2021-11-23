62d51b3a12607e31b3d98031e3af99ceb9c71c33
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2021-11-23 19:10:51 +0200

    title language

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -8,7 +8,7 @@ const Balance = () => {
   const balance = transactions.length > 0 ? new Intl.NumberFormat('uk', {minimumFractionDigits: 2}).format(transactions[0].balance) : "0.00";
   return (
     <div className={styles.wrapper}>
-      <h2 className={styles.title}>Current Balance</h2>
+      <h2 className={styles.title}>ВАШ БАЛАНС</h2>
       <p className={styles.text}>
         <span className={styles.currency}>&#8372;</span>
         {balance}
